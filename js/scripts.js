385fb9c65d304146cab90a8959145c6c7524db57
pchmc72462 <pchmc72462@gmail.com>
2018-08-10 09:23:33 -0700

    Make rest of question divs. Run jQ. No bugs so far.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@ $(document).ready(function() {
   $('form#stack').submit(function(event) {
     event.preventDefault();
     var questionOne=$("#questionOne option:selected").val();
+    var questionTwo=$("#questionTwo option:selected").val();
+    var questionThree=$("#questionThree option:selected").val();
+    var questionFour=$("#questionFour option:selected").val();
+    var questionFive=$("#questionFive option:selected").val();
     console.log(questionOne);
+    console.log(questionTwo);
+    console.log(questionThree);
+    console.log(questionFour);
+    console.log(questionFive);
   });
 });
 });
